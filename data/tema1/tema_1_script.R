31369afab3ee4add8f320e10f0181ea0690d59a4
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2021-03-18 20:58:03 -0600

    Update tema_1_script.R

--- data/tema1/tema_1_script.R
+++ data/tema1/tema_1_script.R
@@ -1,5 +1,8 @@
 #Reading dataset
 auto <- read.csv('./auto-mpg.csv')
 
-#funcion names 
+#funcion names, nombres de las columnas 
 names(auto)
+
+#dataset sin header
+auto_noheader <- read.csv('./auto-mpg-noheader.csv', header = FALSE)
