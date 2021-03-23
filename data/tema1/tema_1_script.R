43e2a2b6da368ea3e8d56f9818f5a7b0a2d30ef1
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2021-03-22 19:53:44 -0600

    Update tema_1_script.R

--- data/tema1/tema_1_script.R
+++ data/tema1/tema_1_script.R
@@ -1,8 +1,17 @@
 #Reading dataset
-auto <- read.csv('./auto-mpg.csv')
+auto <- read.csv('./auto-mpg.csv', stringsAsFactors = FALSE)
 
 #funcion names, nombres de las columnas 
 names(auto)
 
 #dataset sin header
-auto_noheader <- read.csv('./auto-mpg-noheader.csv', header = FALSE)
+auto_noheader <- read.csv('./auto-mpg-noheader.csv', header = FALSE) #columns = [] = c('numero', 'millas por galon', desplazamiento', 'peso', 'aceleracion', 'año', 'modelo')
+
+#Changing column names
+colnames(auto_noheader) <- c('numero', 'millas_por_galon', 'desplazamiento', 'peso', 'aceleracion', 'año', 'modelo')
+
+#loading from a website
+#who_from_internet <- read.csv('https://frogames.es/course/r/intro/tema1/WHO.csv')
+
+#NA Not available
+# na.strings = ''
