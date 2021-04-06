88b969949a0e4fd8f5250d0dfd60b8f2523a6113
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2021-04-05 20:39:05 -0500

    Add files via upload
    fwf_wo_a_column

--- data/tema1/04_analisis_fwf.R
+++ data/tema1/04_analisis_fwf.R
@@ -10,3 +10,9 @@ students_data_header <- read.fwf('./student-fwf-header.txt',
                           header = TRUE,
                           sep = '\t',
                           skip = 2)
+
+#cargar el fwf sin alguna columnas
+students_data_no_email <- read.fwf('./student-fwf.txt', 
+                          widths = c(4,15,-20,15,4),
+                          col.names = c('id', 'nombre', 'carrera', 'año'))
+#menos la longitud de la columna seleccionada y no se especifica el nombre de la misma
