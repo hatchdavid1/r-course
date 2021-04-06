45042acd3bdca3915b03eef8c80e4403bd9cc5f5
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2021-04-05 20:35:59 -0500

    Add files via upload
    fwf_with_header

--- data/tema1/04_analisis_fwf.R
+++ data/tema1/04_analisis_fwf.R
@@ -3,3 +3,10 @@ students_data <- read.fwf('./student-fwf.txt',
                           widths = c(4,15,20,15,4),
                           col.names = c('id', 'nombre', 'email', 'carrera', 'año'))
 #c(, ,) para especificar el ancho de cada columna
+
+#dataset de ancho fijo con header
+students_data_header <- read.fwf('./student-fwf-header.txt', 
+                          widths = c(4,15,20,15,4),
+                          header = TRUE,
+                          sep = '\t',
+                          skip = 2)
